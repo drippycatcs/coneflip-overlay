32178b0c23eb06af78a00632e79cd10722b90a74
DrippyCatCs <65185291+aquaisbalding@users.noreply.github.com>
2025-01-16 16:57:06 +0100

    change any of the images/ sounds u want

--- app.js
+++ app.js
@@ -107,14 +107,13 @@ app.get('/api/cones/add', async (req, res, next) => {
     }
 });
 
-// app.get('/api/cones/duel', (req, res) => {
-//     const name = req.query.name?.toLowerCase().trim() || '';
-//     const name2 = req.query.duel?.toLowerCase().trim() || '';
-//     if (!name) return res.send('Name cannot be blank or invalid.');
-//     io.emit('addCone', name);
-//     io.emit('addCone', name2);
-//     res.sendStatus(200);
-// });
+app.get('/api/cones/duel', (req, res) => {
+   const name = req.query.name?.toLowerCase().trim() || '';
+     const name2 = req.query.duel?.toLowerCase().trim() || '';
+     if (!name) return res.send('Name cannot be blank or invalid.');
+     io.emit("addConeDuel", name, name2);
+    res.sendStatus(200);
+ });
 
 app.get('/api/leaderboard', async (req, res, next) => {
     const name = req.query.name?.toLowerCase().trim() || '';
